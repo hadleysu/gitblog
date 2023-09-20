34d33274d5c57ab68346ac2e90b94d352b5bdcc6
hadley su <143312977+hadleysu@users.noreply.github.com>
2023-09-20 14:11:06 +0800

    fix: bug

--- main.py
+++ main.py
@@ -153,6 +153,7 @@ def add_md_top(repo, md, me):
 def add_md_firends(repo, md, me):
     s = FRIENDS_TABLE_HEAD
     friends_issues = list(repo.get_issues(labels=FRIENDS_LABELS))
+    friends_issue_number = friends_issues[0].number
     for issue in friends_issues:
         for comment in issue.get_comments():
             if is_hearted_by_me(comment, me):
@@ -163,7 +164,9 @@ def add_md_firends(repo, md, me):
                     pass
     s = markdown.markdown(s, output_format="html", extensions=["extra"])
     with open(md, "a+", encoding="utf-8") as md:
-        md.write("## 友情链接\n")
+        md.write(
+            f"## [友情链接](https://github.com/{str(me)}/gitblog/{friends_issue_number})\n"
+        )
         md.write(s)
         md.write("\n\n")
 
@@ -180,8 +183,8 @@ def add_md_recent(repo, md, me, limit=5):
                     count += 1
                     if count >= limit:
                         break
-        except:
-            return
+        except Exception as e:
+            print(str(e))
 
 
 def add_md_header(md, repo_name):
